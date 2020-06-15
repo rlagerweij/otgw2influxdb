24750cb7c549ef58584dc3a662afc0cd284ccbf9
Robbert <rlagerweij@hotmail.com>
2020-06-15 22:33:05 +0200

    add some more tests

--- opentherm_test.go
+++ opentherm_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/hex"
+	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -12,6 +14,7 @@ func TestDecodeF8_8(t *testing.T) {
 	}{
 		{"00FF", "1.00"},
 		{"0000", "0.00"},
+		{"13A0", "19.62"},
 	}
 	for _, test := range testTable {
 		arg, err := hex.DecodeString(test.in)
@@ -22,6 +25,82 @@ func TestDecodeF8_8(t *testing.T) {
 			if actual != test.out {
 				t.Errorf("decodeF8_8(%v) failed: expected %s, got %s", arg, test.out, actual)
 			}
+		}
+	}
+}
+
+func TestDecodeFlag8(t *testing.T) {
+	testTable := []struct {
+		in     string
+		bitNum byte
+		out    string
+	}{
+		{"FFFF", 1, "1"},
+		{"0000", 1, "0"},
+	}
+	for _, test := range testTable {
+		arg, err := hex.DecodeString(test.in)
+		if err != nil {
+			t.Errorf("decodig hex string %s failed: %v", test.in, err)
+		} else {
+			actual := decodeFlag8(arg[0], test.bitNum)
+			if actual != test.out {
+				t.Errorf("decodeFlag(%v) failed: expected %s, got %s", arg, test.out, actual)
+			}
+		}
+	}
+}
+
+func TestDecodeLP(t *testing.T) {
+	var passed bool = true
+	// var testedFunction = *func()
+
+	testTable := []struct {
+		in  string
+		out string
+	}{
+		{"B40193C33", "otgw boiler_water_temp=60.20 "},      // cTypeF8_8
+		{"BC0784750", "otgw burner_operation_hours=18256 "}, //cTypeU16
+	}
+	readConfig("otgw2db.testing.cfg")
+
+	for _, test := range testTable {
+		result := decodeLineProtocol(test.in)
+		parts := strings.SplitAfter(result, " ")
+		actual := fmt.Sprint(parts[0], parts[1])
+
+		if strings.Compare(actual, test.out) != 0 {
+			passed = false
+		}
+		if !passed {
+			t.Errorf("decodeLP(\"%v\") failed: expected %s, got %s", test.in, test.out, actual)
+		}
+	}
+}
+
+func TestConfigWithDecodeLP(t *testing.T) {
+
+	testTable := []struct {
+		in         string
+		settingKey string
+		out        string
+		out2       string
+	}{
+		{"B40193C33", "store_boiler_water_temp", "otgw boiler_water_temp=60.20", ""},
+	}
+
+	readConfig("otgw2db.testing.cfg")
+
+	for _, test := range testTable {
+		config[test.settingKey] = "YES"
+		actual := decodeLineProtocol(test.in)
+		if !strings.Contains(actual, test.out) {
+			t.Errorf("decodeLP(\"%v\") with setting %s failed: expected %s, got %s", test.in, config[test.settingKey], test.out, actual)
+		}
+		config[test.settingKey] = "NO"
+		actual2 := decodeLineProtocol(test.in)
+		if !strings.Contains(actual2, test.out2) {
+			t.Errorf("decodeLP(\"%v\") with setting %s failed: expected %s, got %s", test.in, config[test.settingKey], test.out2, actual2)
 		}
 	}
 }
